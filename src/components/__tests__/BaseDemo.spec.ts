6ee38fa1015e2e7b3afe60525dce6550595792f2
李武帝 <liwudi@saanw.com>
2022-12-15 15:05:53 +0800

    feat: 🎸 添加样式校验+全局公用css

--- src/components/__tests__/BaseDemo.spec.ts
+++ src/components/__tests__/BaseDemo.spec.ts
@@ -11,7 +11,7 @@ describe("base-demo", () => {
 });
 
 describe("base-demo", () => {
-  it("render emits", () => {
+  it("render click", () => {
     const wrapper = mount(BaseDemo, { props: { msg: "hello world" } });
     wrapper.find(".button").trigger("click");
     expect(wrapper.emitted().update).toBeTruthy();
@@ -22,8 +22,8 @@ describe("base-demo", () => {
 describe("base-demo", () => {
   it("render emits", () => {
     const wrapper = mount(BaseDemo, { props: { msg: "hello world" } });
-    wrapper.find(".button").trigger("click");
-    expect(wrapper.emitted().update).toBeTruthy();
+    wrapper.vm.$emit("change", 1);
+    expect(wrapper.emitted().change).toBeTruthy();
   });
 });
 
